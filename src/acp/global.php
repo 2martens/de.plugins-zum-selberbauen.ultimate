d810821f32b56a75c164c83ac33a944f068ca97a
Jim Martens <jim1@live.de>
2011-12-29 14:21:03 +0100

    Added global.php and index.php in the acp directory.

---
+++ src/acp/global.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author Jim Martens
+ * @copyright 2011 Jim Martens
+ * @license http://www.plugins-zum-selberbauen.de/index.php?page=CMSLicense CMS License
+ * @package de.plugins-zum-selberbauen.cms
+ * @category Ultimate CMS
+ */
+//define path
+define('RELATIVE_ULTIMATE_DIR', '../');
+
+//include config
+require_once(dirname(dirname(__FILE__)).'/config.inc.php');
+
+//include wcf
+require_once(RELATIVE_WCF_DIR.'global.php');
+if (!count($packageDirs)) $packageDirs[] = ULTIMATE_DIR;
+$packageDirs[] = WCF_DIR;
+
+//starting ultimate acp
+require_once(WCF_DIR.'lib/system/WCFACP.class.php');
+new wcf\system\WCFACP();
---
+++ src/acp/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author Jim Martens
+ * @copyright 2011 Jim Martens
+ * @license http://www.plugins-zum-selberbauen.de/index.php?page=CMSLicense CMS License
+ * @package de.plugins-zum-selberbauen.cms
+ * @category Ultimate CMS
+ */
+require_once('./global.php');
+wcf\system\request\RequestHandler::getInstance()->handle('ultimate', true);
