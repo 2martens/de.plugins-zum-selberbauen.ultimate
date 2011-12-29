325cee9734fc72b180a84aad6ee85cbb22cf4e6a
Jim Martens <jim1@live.de>
2011-12-29 16:00:55 +0100

    Added basic SiteComponentPage.

---
+++ src/lib/page/SiteComponentPage.class.php
@@ -0,0 +1,21 @@
+<?php
+namespace ultimate\page;
+use ultimate\page\AbstractComponentPage;
+
+/**
+ * Fetches a default site component.
+ *
+ * @author Jim Martens
+ * @copyright 2011 Jim Martens
+ * @license http://www.plugins-zum-selberbauen.de/index.php?page=CMSLicense CMS License
+ * @package de.plugins-zum-selberbauen.cms
+ * @subpackage page
+ * @category Ultimate CMS
+ */
+class SiteComponentPage extends AbstractComponentPage {
+    /**
+     * @see wcf\page\AbstractPage::$templateName
+     */
+    public $templateName = 'site';
+    
+}
---
+++ src/templates/site.tpl
@@ -0,0 +1,11 @@
+<div class="site" id="content-id-{@$contentID}">
+    <div class="mainHeadline">
+        <div class="headlineContainer">
+            <h2>{@$contentTitle}</h2>
+            <p>{@$contentDescription}</p>
+        </div>
+    </div>
+    <div class="border content">
+        <p>{@$contentText}</p>
+    </div>
+</div>
