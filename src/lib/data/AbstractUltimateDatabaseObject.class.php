65c87ea3b597dc5c12fca9151c72b2e83481dcb4
Jim Martens <jim1@live.de>
2011-12-29 13:03:58 +0100

    Added abstract class for Ultimate database objects.

---
+++ src/lib/data/AbstractUltimateDatabaseObject.class.php
@@ -0,0 +1,23 @@
+<?php
+namespace ultimate\data;
+use wcf\data\DatabaseObject;
+
+/**
+ * Every Ultimate data class should extend this class.
+ *
+ * @author Jim Martens
+ * @copyright 2011 Jim Martens
+ * @license http://www.plugins-zum-selberbauen.de/index.php?page=CMSLicense CMS License
+ * @package de.plugins-zum-selberbauen.cms
+ * @subpackage data
+ * @category Ultimate CMS
+ */
+abstract class AbstractUltimateDatabaseObject extends DatabaseObject {
+    
+	/**
+     * @see	wcf\data\IStorableObject::getDatabaseTableName()
+     */
+    public static function getDatabaseTableName() {
+        return 'ultimate'.ULTIMATE_N.'_'.static::$databaseTableName;
+    }
+}
