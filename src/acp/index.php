9b4592d105bc8da7c3d9135f32ddcdc27c3f0da1
Jim Martens <jim1@live.de>
2011-12-29 20:23:16 +0100

    Changed package name to *.ultimate.

--- src/acp/index.php
+++ src/acp/index.php
@@ -3,7 +3,7 @@
  * @author Jim Martens
  * @copyright 2011 Jim Martens
  * @license http://www.plugins-zum-selberbauen.de/index.php?page=CMSLicense CMS License
- * @package de.plugins-zum-selberbauen.cms
+ * @package de.plugins-zum-selberbauen.ultimate
  * @category Ultimate CMS
  */
 require_once('./global.php');
