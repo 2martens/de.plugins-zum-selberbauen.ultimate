919775a9b3181f9eca46067070533e6b3774a843
Jim Martens <jim1@live.de>
2012-01-02 13:54:02 +0100

    Moved parts of code into specific methods.

--- src/lib/system/Dispatcher.class.php
+++ src/lib/system/Dispatcher.class.php
@@ -30,59 +30,41 @@ class Dispatcher extends SingletonFactory {
     protected $requestURI = '';
     
     /**
+     * Contains the domain path of this application.
+     * @var string
+     */
+    protected $domainPath = '';
+    
+    /**
+     * Contains an array of the configurations.
+     * @var array
+     */
+    protected $viewConfigurations = array();
+    
+    /**
+     * Contains an array of the links
+     * @var array
+     */
+    protected $linkList = array();
+    
+    
+    /**
      * Handles a http request.
      */
     public function handle() {
-        if (isset($_GET['request'])) $this->requestURI = FileUtil::removeTrailingSlash(StringUtil::trim($_GET['request']));
-        
-        //loading domain path from cache
-        $cache = 'domain-paths';
-        $file = WCF_DIR.'cache/cache.'.$cache.'.php';
-        $className = 'ultimate\system\cache\builder\ApplicationDomainPathCacheBuilder';
-        CacheHandler::getInstance()->addResource($cache, $file, $className);
-        
-        $domainPaths = CacheHandler::getInstance()->get($cache, 'paths');
-        $domainPath = $domainPaths[PACKAGE_ID];
-        //cut domain path away from request uri
-        
-        $this->requestURI = str_replace($domainPath, '', $this->requestURI);
-        if ($pos = strpos($this->requestURI, 'index.php') !== false) {
-            HeaderUtil::redirect($this->requestURI.'/');
-            exit;
-        } elseif ($this->requestURI == 'index') {
-            new IndexPage();
-            exit;
-        }
-        
-        //loading links from cache
-        $cache = 'ultimate-links-'.PACKAGE_ID;
-        $file = ULTIMATE_DIR.'cache/cache.'.$cache.'.php';
-        $className = 'ultimate\system\cache\builder\UltimateLinksCacheBuilder';
-        CacheHandler::getInstance()->addResource($cache, $file, $className);
-        $linkList = array();
-        $linkList = CacheHandler::getInstance()->get($cache, 'links');
-        
-        if (false) { //does nothing
-            echo $this->getRequestURI().'<br /><pre>'.print_r($linkList, true).'</pre>';
-            //exit;
-        }
-        
-        if (!in_array($this->requestURI, $linkList)) {
-            throw new IllegalLinkException();
-        }
-        //loading configurations from cache
-        $viewConfigurations = array();
-        $viewConfigurations = CacheHandler::getInstance()->get($cache, 'configs');
+        $this->readParameters();
+        $this->readCache();
+        $this->validateRequest();
         
         //$config['templateName'] = template name of the overall generated template
         //$config['content'] = array ('content id' => '*ComponentPage')
-        $config = $viewConfigurations[$this->requestURI];
+        $config = $this->viewConfigurations[$this->requestURI];
         $callData = array(
         	'templateName' => $config['templateName'],
             'content' => array()
         );
         foreach ($config['content'] as $id => $component) {
-            //if statement does nothing
+            //if statement does nothing @todo remove for production usage
             if (false) echo $component.'<br /><pre>'.print_r(UltimateCore::getTPL()->templatePaths, true).'</pre>';
             
             require_once(ULTIMATE_DIR.'lib/page/'.$component.'.class.php');
@@ -91,7 +73,6 @@ class Dispatcher extends SingletonFactory {
             $result = $obj->getOutput(); //returns output
             $callData['content'][$id] = $result;
         }
-        
         
         $controllerObj = 'ultimate\page\GenericCMSPage';
         new $controllerObj($callData);
@@ -102,6 +83,61 @@ class Dispatcher extends SingletonFactory {
      */
     public function getRequestURI() {
         return $this->requestURI;
+    }
+    
+    /**
+     * Reads the given parameters.
+     */
+    protected function readParameters() {
+        if (isset($_GET['request'])) $this->requestURI = FileUtil::removeTrailingSlash(StringUtil::trim($_GET['request']));
+    }
+    
+    /**
+     * Reads the cache for this class.
+     */
+    protected function readCache() {
+        //loading domain path from cache
+        $cache = 'domain-paths';
+        $file = WCF_DIR.'cache/cache.'.$cache.'.php';
+        $className = 'ultimate\system\cache\builder\ApplicationDomainPathCacheBuilder';
+        CacheHandler::getInstance()->addResource($cache, $file, $className);
+        
+        $domainPaths = CacheHandler::getInstance()->get($cache, 'paths');
+        $this->domainPath = $domainPaths[PACKAGE_ID];
+        
+        //loading configurations from cache
+        $this->viewConfigurations = CacheHandler::getInstance()->get($cache, 'configs');
+        
+        //loading links from cache
+        $cache = 'ultimate-links-'.PACKAGE_ID;
+        $file = ULTIMATE_DIR.'cache/cache.'.$cache.'.php';
+        $className = 'ultimate\system\cache\builder\UltimateLinksCacheBuilder';
+        CacheHandler::getInstance()->addResource($cache, $file, $className);
+        $this->linkList = CacheHandler::getInstance()->get($cache, 'links');
+    }
+    
+    /**
+     * Validates the given request uri.
+     * @throws IllegalLinkException
+     */
+    protected function validateRequest() {
+        $this->requestURI = str_replace($this->domainPath, '', $this->requestURI);
+        if ($pos = strpos($this->requestURI, 'index.php') !== false) {
+            HeaderUtil::redirect($this->requestURI.'/');
+            exit;
+        } elseif ($this->requestURI == 'index') {
+            new IndexPage();
+            exit;
+        }
+        //@todo remove for production later
+        if (false) { //does nothing
+            echo $this->getRequestURI().'<br /><pre>'.print_r($this->linkList, true).'</pre>';
+            //exit;
+        }
+        
+        if (!in_array($this->requestURI, $this->linkList)) {
+            throw new IllegalLinkException();
+        }
     }
     
 }
