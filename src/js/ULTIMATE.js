96c3cf5169a8d5596458932d858ec0fbb071644b
Jim Martens <jim1@live.de>
2012-01-04 20:02:56 +0100

    Began the addEntry method.

--- src/js/ULTIMATE.js
+++ src/js/ULTIMATE.js
@@ -10,3 +10,26 @@
  * Initialize the UTLIMATE namespace.
  */
 var ULTIMATE = {};
+
+/**
+ * ConfigEditor API
+ */
+ULTIMATE.ConfigEditor = function() { this.init(); };
+ULTIMATE.ConfigEditor.prototype = {
+	/**
+	 * Contains the current selector.
+	 */
+	_selector: null,
+		
+	/**
+	 * Initializes the ConfigEditor API.
+	 */
+	init: function() {
+		
+	},	
+
+	addEntry: function(selector, id) {
+		this._selector = selector;
+		
+	}
+};
