683feed237005365d30386d8a3a731389ef6b5d1
Jim Martens <jim1@live.de>
2012-01-06 13:45:54 +0100

    Added core functions to ULTIMATE object and added JSON api.

--- src/js/ULTIMATE.js
+++ src/js/ULTIMATE.js
@@ -12,6 +12,80 @@
 var ULTIMATE = {};
 
 /**
+ * ULTIMATE Content Management System core methods
+ */
+$.extend(ULTIMATE, {
+	
+	/**
+	 * Counts the elements in an array-like object.
+	 * 
+	 * @param 	Object 	object
+	 * @return 	integer
+	 */
+	countArrayLikeObj: function(object) {
+		var length = 0;
+		if (typeof object != 'object') return null;
+		$.each(object, function(key, value) {
+			length++;
+		});
+		return length;
+	}
+});
+
+/**
+ * JSON API
+ */
+ULTIMATE.JSON = {
+	
+    /**
+     * Encodes a given variable.
+     * 
+     * @param 	Object variable
+     * @return 	string
+     */
+	encode: function(variable) {
+		var type = typeof variable;
+		
+		if (variable instanceof Array) {
+			var output = '[';
+			var index = 0;
+			var length = variable.length;
+			$.each(variable, function(key, value) {
+				if (index > 0 && index < length) output += ',';
+				if (value instanceof Array || typeof value == 'object') {
+					output += this.encode(value);
+				}
+				output += '"' + value + '"';								
+				index++;
+			});
+			output += ']';
+			return output;
+		} else if (type == 'object') {
+			var output = '{';
+			var index = 0;
+			var length = ULTIMATE.countArrayLikeObj(variable);
+			$.each(variable, function(key, value) {
+				if (index > 0 && index < length) output += ',';
+				output += '"' + key + '":';
+				var valType = typeof value;
+				if (value instanceof Array) {
+					output += this.encode(value);
+				} else if (valType == 'object') {
+					output += this.encode(value);
+				} else if (valType == 'string') {
+					output += '"' + value + '"';
+				}
+				index++;
+			});
+			
+			output += '}';
+			return output;
+		}
+		return null;		
+	}
+};
+
+/**
  * ConfigEditor API
  */
 ULTIMATE.ConfigEditor = {
