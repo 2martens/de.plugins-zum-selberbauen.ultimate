c2f945de3057f290fa726db5c930f08e21c1a72c
Jim Martens <jim1@live.de>
2012-01-07 15:40:07 +0100

    Added UltimateLinkDeleteAction.

---
+++ src/lib/acp/action/UltimateLinkDeleteAction.class.php
@@ -0,0 +1,61 @@
+<?php
+namespace ultimate\acp\action;
+use wcf\util\HeaderUtil;
+
+use ultimate\data\link\LinkAction;
+use wcf\action\AbstractSecureAction;
+
+/**
+ * Deleted the specified link.
+ *
+ * @author Jim Martens
+ * @copyright 2011-2012 Jim Martens
+ * @license http://www.plugins-zum-selberbauen.de/index.php?page=CMSLicense CMS License
+ * @package de.plugins-zum-selberbauen.ultimate
+ * @subpackage acp.action
+ * @category Ultimate CMS
+ */
+class UltimateLinkDeleteAction extends AbstractSecureAction {
+    
+    /**
+     * @see \wcf\action\AbstractAction::$neededPermissions
+     */
+    public $neededPermissions = array(
+        'admin.content.ultimate.canDeleteLink'
+    );
+
+    /**
+     * Contains the link id.
+     * @var int
+     */
+    protected $linkID = 0;
+    
+    /**
+     * Contains the redirect url.
+     * @var string
+     */
+    protected $url = '';
+    
+    /**
+     * @see \wcf\action\IAction::readParameters()
+     */
+    public function readParameters() {
+        parent::readParameters();
+        if (isset($_GET['id'])) $this->linkID = intval($_GET['id']);
+        if (isset($_GET['url'])) $this->url = trim($_GET['url']);
+    }
+    
+    /**
+     * @see \wcf\action\IAction::execute()
+     */
+    public function execute() {
+        parent::execute();
+        $linkAction = new LinkAction(array($this->linkID), 'delete');
+        $linkAction->executeAction();
+        $this->executed();
+        
+        //redirecting back to referrer
+        HeaderUtil::redirect($this->url);
+        exit;
+    }
+}
