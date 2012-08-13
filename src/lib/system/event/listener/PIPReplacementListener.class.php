806e25d8273e2bc953dd6e50a689caaf2f787e31
Jim Martens <jim1@live.de>
2012-08-13 15:37:11 +0200

    Changed spaces into tabs for leading, spaces for correcting indentation.

--- src/lib/system/event/listener/PIPReplacementListener.class.php
+++ src/lib/system/event/listener/PIPReplacementListener.class.php
@@ -9,82 +9,81 @@ use wcf\system\WCF;
  * 
  * This PIP-like listener is called on each installation, update or uninstallation of a Ultimate CMS plugin.
  * 
- * @author Jim Martens
- * @copyright 2011-2012 Jim Martens
- * @license http://www.plugins-zum-selberbauen.de/index.php?page=CMSLicense CMS License
- * @package de.plugins-zum-selberbauen.ultimate
- * @subpackage system.event.listener
- * @category Ultimate CMS
+ * @author		Jim Martens
+ * @copyright	2011-2012 Jim Martens
+ * @license		http://www.plugins-zum-selberbauen.de/index.php?page=CMSLicense CMS License
+ * @package		de.plugins-zum-selberbauen.ultimate
+ * @subpackage	system.event.listener
+ * @category	Ultimate CMS
  */
 class PIPReplacementListener implements IEventListener {
-    
-    /**
-     * @see \wcf\system\event\IEventListener::execute()
-     */
-    public function execute($eventObj, $className, $eventName) {
-        
-        if ($className == 'BlockTypePackageInstallationPlugin') {
-            $this->{$eventName}($eventObj);
-        }
-    }
-    
-    /**
-     * Checks for specific requirements and sets values.
-     * 
-     * @param \wcf\system\package\plugin\BlockTypePackageInstallationPlugin $eventObj
-     */
-    protected function install(\wcf\system\package\plugin\BlockTypePackageInstallationPlugin $eventObj) {
-        $archive = $eventObj->installation->getArchive();
-        
-        // checks plugin tag - if package is not plugin of Ultimate CMS -> throw exception
-        if ($archive->getPackageInfo('plugin') != 'de.plugins-zum-selberbauen.ultimate') {
-            throw new SystemException('You can\'t use this PIP if you\'re not a plugin of the Ultimate CMS.');
-        }
-        
-        $eventObj->className = '\ultimate\data\blocktype\BlockTypeEditor';
-    }
-    
-    /**
-     * Fills the prepared variable in the PIP the sql data.
-     * 
-     * @param \wcf\system\package\plugin\BlockTypePackageInstallationPlugin $eventObj
-     */
-    protected function findExistingItem(\wcf\system\package\plugin\BlockTypePackageInstallationPlugin $eventObj) {
-        $sqlData['sql'] = 'SELECT    blockTypeID, packageID, blockTypeName, blockTypeClassName
-                           FROM      ultimate'.ULTIMATE_N.'_blocktype
-                           WHERE     packageID     = ?
-                           AND       blockTypeName = ?';
-        $sqlData['parameters'] = array(
-            $eventObj->installation->getPackageID(), 
-            $eventObj->findExistingItemData['blockTypeName']
-        );
-        $eventObj->findExistingItemSQLData = $sqlData;
-    }
-    
-    /**
-     * Checks if uninstallation is possible. If true, the specific variable in the PIP is set appropriate.
-     * 
-     * @param \wcf\system\package\plugin\BlockTypePackageInstallationPlugin $eventObj
-     */
-    protected function hasUninstall(\wcf\system\package\plugin\BlockTypePackageInstallationPlugin $eventObj) {
-        $sql = 'SELECT	COUNT(*) AS count
-                FROM	ultimate'.ULTIMATE_N.'_blocktype
-                WHERE	packageID = ?';
-        $statement = WCF::getDB()->prepareStatement($sql);
-        $statement->execute(array($eventObj->installation->getPackageID()));
-        $installationCount = $statement->fetchArray();
-        $eventObj->hasUninstallReturn = ($installationCount['count'] > 0);
-    }
-    
-    /**
-     * Uninstalls the fitting block types.
-     * 
-     * @param \wcf\system\package\plugin\BlockTypePackageInstallationPlugin $eventObj
-     */
-    protected function uninstall(\wcf\system\package\plugin\BlockTypePackageInstallationPlugin $eventObj) {
-        $sql = 'DELETE FROM	ultimate'.ULTIMATE_N.'_blocktype
-                WHERE		packageID = ?';
-        $statement = WCF::getDB()->prepareStatement($sql);
-        $statement->execute(array($eventObj->installation->getPackageID()));
-    }
+	/**
+	 * @see	\wcf\system\event\IEventListener::execute()
+	 */
+	public function execute($eventObj, $className, $eventName) {
+		
+		if ($className == 'BlockTypePackageInstallationPlugin') {
+			$this->{$eventName}($eventObj);
+		}
+	}
+	
+	/**
+	 * Checks for specific requirements and sets values.
+	 * 
+	 * @param	\wcf\system\package\plugin\BlockTypePackageInstallationPlugin	$eventObj
+	 */
+	protected function install(\wcf\system\package\plugin\BlockTypePackageInstallationPlugin $eventObj) {
+		$archive = $eventObj->installation->getArchive();
+		
+		// checks plugin tag - if package is not plugin of Ultimate CMS -> throw exception
+		if ($archive->getPackageInfo('plugin') != 'de.plugins-zum-selberbauen.ultimate') {
+			throw new SystemException('You can\'t use this PIP if you\'re not a plugin of the Ultimate CMS.');
+		}
+		
+		$eventObj->className = '\ultimate\data\blocktype\BlockTypeEditor';
+	}
+	
+	/**
+	 * Fills the prepared variable in the PIP the sql data.
+	 * 
+	 * @param \wcf\system\package\plugin\BlockTypePackageInstallationPlugin $eventObj
+	 */
+	protected function findExistingItem(\wcf\system\package\plugin\BlockTypePackageInstallationPlugin $eventObj) {
+		$sqlData['sql'] = 'SELECT   blockTypeID, packageID, blockTypeName, blockTypeClassName
+		                   FROM     ultimate'.ULTIMATE_N.'_blocktype
+		                   WHERE    packageID     = ?
+		                   AND      blockTypeName = ?';
+		$sqlData['parameters'] = array(
+			$eventObj->installation->getPackageID(), 
+			$eventObj->findExistingItemData['blockTypeName']
+		);
+		$eventObj->findExistingItemSQLData = $sqlData;
+	}
+	
+	/**
+	 * Checks if uninstallation is possible. If true, the specific variable in the PIP is set appropriate.
+	 * 
+	 * @param	\wcf\system\package\plugin\BlockTypePackageInstallationPlugin	$eventObj
+	 */
+	protected function hasUninstall(\wcf\system\package\plugin\BlockTypePackageInstallationPlugin $eventObj) {
+		$sql = 'SELECT  COUNT(*) AS count
+		        FROM    ultimate'.ULTIMATE_N.'_blocktype
+		        WHERE   packageID = ?';
+		$statement = WCF::getDB()->prepareStatement($sql);
+		$statement->execute(array($eventObj->installation->getPackageID()));
+		$installationCount = $statement->fetchArray();
+		$eventObj->hasUninstallReturn = ($installationCount['count'] > 0);
+	}
+	
+	/**
+	 * Uninstalls the fitting block types.
+	 * 
+	 * @param	\wcf\system\package\plugin\BlockTypePackageInstallationPlugin	$eventObj
+	 */
+	protected function uninstall(\wcf\system\package\plugin\BlockTypePackageInstallationPlugin $eventObj) {
+		$sql = 'DELETE FROM	ultimate'.ULTIMATE_N.'_blocktype
+		        WHERE       packageID = ?';
+		$statement = WCF::getDB()->prepareStatement($sql);
+		$statement->execute(array($eventObj->installation->getPackageID()));
+	}
 }
