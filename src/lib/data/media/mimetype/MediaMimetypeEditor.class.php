ca6bf5a31de029fb6c72e62357750c0ae7d57f7d
Jim Martens <jim1@live.de>
2012-09-18 15:21:24 +0200

    Added mediaProvider and mediaMimetypes.

--- src/lib/data/media/mimetype/MediaMimetypeEditor.class.php
+++ src/lib/data/media/mimetype/MediaMimetypeEditor.class.php
@@ -8,19 +8,19 @@ use wcf\data\IEditableCachedObject;
  * 
  * @author		Jim Martens
  * @copyright	2011-2012 Jim Martens
- * @license		http://www.plugins-zum-selberbauen.de/index.php?page=CMSLicense CMS License
+ * @license		http://www.gnu.org/licenses/lgpl-3.0 GNU Lesser General Public License, version 3
  * @package		de.plugins-zum-selberbauen.ultimate
  * @subpackage	data.media.mimetype
  * @category	Ultimate CMS
  */
 class MediaMimetypeEditor extends DatabaseObjectEditor implements IEditableCachedObject {
 	/**
-	 * @see	\wcf\data\DatabaseObjectDecorator::$baseClass
+	 * @link	http://doc.codingcorner.info/WoltLab-WCFSetup/classes/wcf.data.DatabaseObjectDecorator.html#$baseClass
 	 */
 	protected static $baseClass = '\ultimate\data\media\mimetype\Mimetype';
 	
 	/**
-	 * @see	\wcf\data\IEditableCachedObject::resetCache()
+	 * @link	http://doc.codingcorner.info/WoltLab-WCFSetup/classes/wcf.data.IEditableCachedObject.html#resetCache
 	 */
 	public static function resetCache() {
 		CacheHandler::getInstance()->clear(ULTIMATE_DIR.'cache/', 'cache.mimetype.php');
