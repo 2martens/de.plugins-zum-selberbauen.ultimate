5e0e9f9cae900577bef9f7b0d7705d1003dc1dc4
Jim Martens <jim1@live.de>
2012-12-09 19:59:00 +0100

    Added test config.

---
+++ tests/config.inc.php
@@ -0,0 +1,6 @@
+<?php
+if (!defined('TEST_PATH')) {
+	define('TEST_PATH', dirname(dirname(__FILE__)).'/');
+}
+// Please enter here the absolute path to a working ULTIMATE installation.
+require_once('D:/Users/Jim-Standard/Documents/MeineWebsite/woltlab/ultimate/global.php');
