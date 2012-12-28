65295315de214ff3c1ddf77a614f8ac1be8f3c01
Jim Martens <jim1@live.de>
2012-12-28 19:35:08 +0100

    Added custom route class.

---
+++ src/lib/system/request/Route.class.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Contains the Route class.
+ * 
+ * LICENSE:
+ * This file is part of the Ultimate CMS.
+ *
+ * The Ultimate CMS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * The Ultimate CMS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Ultimate CMS.  If not, see {@link http://www.gnu.org/licenses/}.
+ * 
+ * @author		Jim Martens
+ * @copyright	2011-2012 Jim Martens
+ * @license		http://www.gnu.org/licenses/lgpl-3.0 GNU Lesser General Public License, version 3
+ * @package		de.plugins-zum-selberbauen.ultimate
+ * @subpackage	system.request
+ * @category	Ultimate CMS
+ */
+namespace ultimate\system\request;
+
+/**
+ * Extends the WCF-Route class with needed parts.
+ * 
+ * @author		Jim Martens
+ * @copyright	2012 Jim Martens
+ * @license		http://www.gnu.org/licenses/lgpl-3.0 GNU Lesser General Public License, version 3
+ * @package		de.plugins-zum-selberbauen.ultimate
+ * @subpackage	system.request
+ * @category	Ultimate CMS
+ */
+class Route extends \wcf\system\request\Route {
+	/**
+	 * Returns non-empty URL components.
+	 *
+	 * @param	string		$requestURL
+	 * @return	array
+	 */
+	protected function getParts($requestURL) {
+		$urlParts = preg_split('~(\/|\_|\.)~', $requestURL);
+		foreach ($urlParts as $index => $part) {
+			if (empty($part)) {
+				unset($urlParts[$index]);
+			}
+		}
+		
+		// re-index parts
+		return array_values($urlParts);
+	}
+}
