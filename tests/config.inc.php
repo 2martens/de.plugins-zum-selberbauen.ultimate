e3fa30af5161261c55bbb9253cd8b86741f76a96
Jim Martens <jim1@live.de>
2013-05-18 20:03:18 +0200

    Deleted test folder.

--- tests/config.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!defined('TEST_DIR')) {
-	define('TEST_DIR', dirname(__FILE__).'/');
-}
-require_once(TEST_DIR.'../src/global.php');
-require_once(TEST_DIR.'UltimateTestCore.class.php');
-spl_autoload_register(array('ultimate\tests\UltimateTestCore', 'autoload'));
-
-new \ultimate\tests\UltimateTestCore();
--- tests/databaseTablesConfig.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-$testedTables = array(
-	'ultimate1_category',
-	'ultimate1_content',
-	'ultimate1_content_to_category',
-	'ultimate1_layout'
-);
