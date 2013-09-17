89eac20dc30f5db9c82444e172c0a1338ee65be7
Jim Martens <jim1@live.de>
2013-09-17 15:10:37 +0200

    Fixes #121.

--- src/lib/system/dashboard/box/LatestContentsContentDashboardBox.class.php
+++ src/lib/system/dashboard/box/LatestContentsContentDashboardBox.class.php
@@ -108,6 +108,7 @@ class LatestContentsContentDashboardBox extends AbstractContentDashboardBox {
 			$remainingContents[$contentID] = $content;
 			$i++;
 		}
+		$this->contents = $remainingContents;
 		
 		foreach ($this->contents as $content) {
 			$content->authorProfile = new UserProfile($content->__get('author'));
