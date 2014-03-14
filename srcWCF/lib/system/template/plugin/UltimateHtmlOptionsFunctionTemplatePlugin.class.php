cc706daff1f5294ea0cf0c170562dd54bd3a643c
Jim Martens <jim1@live.de>
2014-03-14 14:53:23 +0100

    Updated copyright notices in srcWCF

--- srcWCF/lib/system/template/plugin/UltimateHtmlOptionsFunctionTemplatePlugin.class.php
+++ srcWCF/lib/system/template/plugin/UltimateHtmlOptionsFunctionTemplatePlugin.class.php
@@ -19,7 +19,7 @@
  * along with the Ultimate CMS. If not, see {@link http://www.gnu.org/licenses/}.
  * 
  * @author		Jim Martens
- * @copyright	2011-2013 Jim Martens
+ * @copyright	2011-2014 Jim Martens
  * @license		http://www.gnu.org/licenses/lgpl-3.0 GNU Lesser General Public License, version 3
  * @package		de.plugins-zum-selberbauen.ultimate
  * @subpackage	system.template.plugin
@@ -43,7 +43,7 @@ use ultimate\data\page\Page;
  * * {ultimateHtmlOptions object=$databaseObjectList selected=$foo}
  * 
  * @author		Jim Martens
- * @copyright	2011-2013 Jim Martens
+ * @copyright	2011-2014 Jim Martens
  * @license		http://www.gnu.org/licenses/lgpl-3.0 GNU Lesser General Public License, version 3
  * @package		de.plugins-zum-selberbauen.ultimate
  * @subpackage	system.template.plugin
